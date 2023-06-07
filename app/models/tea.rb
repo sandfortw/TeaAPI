19dc8f0968a84d63f03362f9f5526d524137d611
Weston Sandfort <sandfortw@gmail.com>
2023-06-07 15:46:03 -0600

    add gem for shoulda matchers, and add validations for tea model

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,2 +1,6 @@
 class Tea < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :temperature, presence: true, numericality: { greater_than: 0 }
+  validates :brew_time, presence: true, numericality: { greater_than: 0 }
 end
