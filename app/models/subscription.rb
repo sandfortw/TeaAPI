f7b7066cd679f0d49594e11a4cbf02fc955308ff
Weston Sandfort <sandfortw@gmail.com>
2023-06-07 16:32:16 -0600

    add validations and relationships to subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,11 @@
 class Subscription < ApplicationRecord
   enum status: [:inactive, :active, :canceled]
   enum frequency: [:weekly, :monthly, :yearly]
+  validates_presence_of :status, :frequency
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :title, presence: true, uniqueness: true
 
-  
+
+  has_many :tea_subscriptions
+  has_many :teas, through: :tea_subscriptions
 end
