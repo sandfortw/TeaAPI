9216e583766e5b6ce2af4131137304ad9e7854d0
Weston Sandfort <sandfortw@gmail.com>
2023-06-07 16:20:30 -0600

    create model validations tests for customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+
+    it { should validate_presence_of(:address) }
+
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+  end
+
+  describe 'relationships' do
+    it { should have_many(:subscriptions) }
+    it { should have_many(:teas).through(:subscriptions) }
+  end
 end
