e93076e653ae6c84dfdb74223857c1e9c76e1780
Weston Sandfort <sandfortw@gmail.com>
2023-06-07 16:21:08 -0600

    add validations to customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < ApplicationRecord
+  validates_presence_of :first_name, :last_name, :address
+  validates :email, presence: true, uniqueness: true
+
+  has_many :subscriptions
+  has_many :teas, through: :subscriptions
 end
