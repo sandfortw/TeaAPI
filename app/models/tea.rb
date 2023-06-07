bd44c8e65229014c75774885f502512b0517d553
Weston Sandfort <sandfortw@gmail.com>
2023-06-07 16:24:39 -0600

    add has_many relationships to tea model

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -3,4 +3,7 @@ class Tea < ApplicationRecord
   validates :description, presence: true
   validates :temperature, presence: true, numericality: { greater_than: 0 }
   validates :brew_time, presence: true, numericality: { greater_than: 0 }
+
+  has_many :tea_subscriptions
+  has_many :subscriptions, through: :tea_subscriptions
 end
