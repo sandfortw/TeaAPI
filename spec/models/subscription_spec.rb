17c699702b03796b46a93bf914f5687b912a9178
Weston Sandfort <sandfortw@gmail.com>
2023-06-07 16:31:23 -0600

    add tests for subscriptions

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:frequency) }
+    it { should validate_presence_of(:price) }
+    it { should validate_numericality_of(:price).is_greater_than(0) }
+    it { should validate_presence_of(:title) }
+    it { should validate_uniqueness_of(:title) }
+  end
+
+  describe 'relationships' do
+    it { should have_many(:tea_subscriptions) }
+    it { should have_many(:teas).through(:tea_subscriptions) }
+  end
 end
