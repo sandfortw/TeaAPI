9c6316803e33e4ede823eca589c090e4d2d32aef
Weston Sandfort <sandfortw@gmail.com>
2023-06-07 16:32:57 -0600

    update schema for new tea subscription join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_07_212422) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_07_221631) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,8 +28,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_212422) do
     t.integer "price"
     t.integer "status", default: 0
     t.integer "frequency", default: 0
+    t.bigint "customer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_subscriptions_on_customer_id"
+  end
+
+  create_table "tea_subscriptions", force: :cascade do |t|
+    t.bigint "tea_id"
+    t.bigint "subscription_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["subscription_id"], name: "index_tea_subscriptions_on_subscription_id"
+    t.index ["tea_id"], name: "index_tea_subscriptions_on_tea_id"
   end
 
   create_table "teas", force: :cascade do |t|
@@ -41,4 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_07_212422) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "subscriptions", "customers"
+  add_foreign_key "tea_subscriptions", "subscriptions"
+  add_foreign_key "tea_subscriptions", "teas"
 end
