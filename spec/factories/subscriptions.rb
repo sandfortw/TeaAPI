7506e1828d69e60878b7687f19cd04b6ae912e4b
Weston Sandfort <sandfortw@gmail.com>
2023-06-08 10:26:31 -0600

    create factories

---
+++ spec/factories/subscriptions.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :subscription do
+    title { Faker::Subscription.plan }
+    price { Faker::Number.decimal(l_digits: 2) }
+    status { Faker::Number.between(from: 0, to: 2) }
+    frequency { Faker::Number.between(from: 0, to: 2) }
+  end
+end
