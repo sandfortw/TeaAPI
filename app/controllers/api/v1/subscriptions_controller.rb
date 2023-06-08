452ba2c45c8b795c92c43d6b09e6d901ccb35815
Weston Sandfort <sandfortw@gmail.com>
2023-06-08 16:58:57 -0600

    update subscriptions controller

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -6,6 +6,19 @@ module Api
         subscriptions = customer.subscriptions
         render json: subscriptions
       end
+
+      def create
+        customer = Customer.find(params[:customer_id])
+        subscription = Subscription.find(params[:subscription_id])
+        customer.subscriptions << subscription
+        render json: customer.subscriptions
+      end
+
+      def destroy
+        customer = Customer.find(params[:customer_id])
+        deleted = customer.subscriptions.delete(params[:subscription_id])
+        render json: deleted
+      end
     end
   end
 end
