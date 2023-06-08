fc045f84d5fd921d6e5393883cac5dec1d473368
Weston Sandfort <sandfortw@gmail.com>
2023-06-08 09:53:22 -0600

    add customer_subscription model and update existing models

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -7,6 +7,8 @@ class Subscription < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :title, presence: true, uniqueness: true
 
+  has_many :customer_subscriptions
+  has_many :customers, through: :customer_subscriptions
   has_many :tea_subscriptions
   has_many :teas, through: :tea_subscriptions
 end
