d842f398735d5ee4f45a2323bfafc80e84e9c69c
Weston Sandfort <sandfortw@gmail.com>
2023-06-08 19:38:04 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customers, only: :show do
-        resources :subscriptions, only: %i[index create destroy]
+        resources :subscriptions, only: %i[index destroy] do
+          post '/', to: 'subscriptions#create'
+        end
       end
     end
   end
