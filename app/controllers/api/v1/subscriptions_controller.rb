5d3fe7d614531fc82a9523e29afd8eb0a10adbbe
Weston Sandfort <sandfortw@gmail.com>
2023-06-08 19:38:55 -0600

    add error handling to subscription controller

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -7,6 +7,8 @@ module Api
         customer = Customer.find(params[:customer_id])
         subscriptions = customer.subscriptions
         render json: SubscriptionSerializer.new(subscriptions).serializable_hash
+      rescue ActiveRecord::RecordNotFound
+        render json: { error: 'Customer not found' }, status: :not_found
       end
 
       def create
@@ -14,12 +16,16 @@ module Api
         subscription = Subscription.find(params[:subscription_id])
         customer.subscriptions << subscription
         render json: SubscriptionSerializer.new(subscription).serializable_hash
+      rescue ActiveRecord::RecordNotFound => e
+        render json: { error: e.message }, status: :not_found
       end
 
       def destroy
         customer = Customer.find(params[:customer_id])
         deleted = customer.subscriptions.delete(params[:id])
-        render json: SubscriptionSerializer.new(deleted).serializable_hash
+        render json: SubscriptionSerializer.new(deleted.first).serializable_hash
+      rescue ActiveRecord::RecordNotFound => e
+        render json: { error: e.message }, status: :not_found
       end
     end
   end
