205ebf9cfa7585886eea702528b2259f3c6a1ca2
Weston Sandfort <sandfortw@gmail.com>
2023-06-08 18:40:41 -0600

    remove requirement for unique title from subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -5,7 +5,7 @@ class Subscription < ApplicationRecord
   enum frequency: %i[weekly monthly yearly]
   validates_presence_of :status, :frequency
   validates :price, presence: true, numericality: { greater_than: 0 }
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true
 
   has_many :customer_subscriptions
   has_many :customers, through: :customer_subscriptions
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe Subscription, type: :model do
     it { should validate_presence_of(:price) }
     it { should validate_numericality_of(:price).is_greater_than(0) }
     it { should validate_presence_of(:title) }
-    it { should validate_uniqueness_of(:title) }
   end
 
   describe 'relationships' do
